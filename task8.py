b81a5775f607df25cbcc0972393837d86402a9dc
batyrbek <batyrbek071194@gmail.com>
2019-12-21 14:13:39 +0600

    task8

---
+++ task8.py
@@ -0,0 +1,11 @@
+# 8. Given two integers A and B 
+# (На английском языке что бы Вы научились понимать A ≤ B).
+#  Print all numbers from A to B inclusively.
+
+# a,b = 1, 100
+# if a < b:
+#     for i in range(a, b + 1):
+#         print(i)
+# else:
+#     for i in range(a, b - 1,-1):
+#         print(i)
