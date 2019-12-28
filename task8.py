df448b8a8ecbfa714df1944a0b5a0e646ca6e504
batyrbek07 <59105885+batyrbek07@users.noreply.github.com>
2019-12-28 14:16:37 +0600

    Update task8.py

--- task8.py
+++ task8.py
@@ -2,10 +2,10 @@
 # (На английском языке что бы Вы научились понимать A ≤ B).
 #  Print all numbers from A to B inclusively.
 
-# a,b = 1, 100
-# if a < b:
-#     for i in range(a, b + 1):
-#         print(i)
-# else:
-#     for i in range(a, b - 1,-1):
-#         print(i)
+a,b = 1, 100
+if a < b:
+    for i in range(a, b + 1):
+        print(i)
+else:
+    for i in range(a, b - 1,-1):
+        print(i)
